4d0ead1a142a38e4ca4ef07004ad9a217a65b203
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-30 18:44:58 +0600

    CHANGE api

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://book-catalogue-backend-three.vercel.app/api/v1",
   }),
   tagTypes: ["users", "books"],
   endpoints: () => ({}),
